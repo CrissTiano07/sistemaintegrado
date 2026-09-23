99c8f6e5b0e9ef3673bda1f2c23665d713295003
CrissTiano07 <tianocmo@gmail.com>
2026-09-23 02:07:06 -0300

    Fix formatting of status message lines

--- nit.js
+++ nit.js
@@ -1524,7 +1524,7 @@ const NitData = {
 
             const linhas = [
                 `${emoji} *STATUS SEMAFÓRICO* ${emoji}`,
-                `---------- ${data} - ${hora} ----------`,
+                `--------- ${data} - ${hora} ---------`,
                 '',
                 `*Ocorrências (${totalOcorr})*`,
                 `*Pendentes (${totalPend})* ${emojiPendentes}`,  // ← USANDO emojiPendentes
@@ -1536,12 +1536,17 @@ const NitData = {
                 if (amc.length) { linhas.push(`🚔🔵 *AMC (${amc.length}):*`); amc.forEach(c => linhas.push(fmt(c))); linhas.push(''); }
 
                 linhas.push('---');
+                linhas.push('');
                 linhas.push(`⏳ *PENDENTES / OUTROS MOTIVOS*`);
+                linhas.push('');
                 linhas.push(`*- Aguardando atendimento:*`);
+                linhas.push('');
                 espera.forEach(c => linhas.push(fmt(c)));
 
                 if (sn.length) {
+                    linhas.push('');
                     linhas.push(`*- Sem necessidade de operação:*`);
+                    linhas.push('');
                     sn.forEach(c => linhas.push(fmt(c)));
                 }
             }
@@ -1549,6 +1554,7 @@ const NitData = {
             if (incluirNorm && norm.length) {
                 linhas.push('');
                 linhas.push(`✅ *NORMALIZADOS (${norm.length}):*`);
+                linhas.push('');
                 norm.forEach(c => linhas.push(fmt(c)));
             }
 
